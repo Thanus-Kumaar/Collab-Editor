e8b9b55f97cbc79749c99a72ced60c12fa0c8c76
Thanus Kumaar <thanuskumaara@gmail.com>
2024-11-25 23:12:00 +0530

    Added basic frontend structure to the project and added tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div className="bg-blue-200 flex flex-row">
+      <div>
+        Hello
+      </div>
+      <div>
+        Amigos
+      </div>
+    </div>
+  )
+}
+
+export default App
